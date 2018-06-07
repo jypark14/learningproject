c742f64c685714bf08edda7bd67c72356f3ebd12
Jeyoung Park <jeyoungp@andrew.cmu.edu>
2018-06-07 17:02:46 -0400

    added 3 test cases, 2 fails

--- __tests__/counter.js
+++ __tests__/counter.js
@@ -3,18 +3,17 @@ import "react-native";
 import React from "react";
 import renderer from "react-test-renderer";
 import { Learning_Project } from "../src/components/counter";
-
+import { CounterReducer } from "../src/reducers/counter";
+import {
+  counterIncrement,
+  counterDecrement,
+  counterIncrement2,
+  counterDecrement2
+} from "../src/actions/counter";
 describe("Stateless Components Render", () => {
   const stringProp = "test";
 
-  //   it("Counter render", () => {
-  //     renderer.create(
-  //       <Learning_Project
-  //         count={80}
-  //         count2={70}
-  //         // onPress={emptyFunc}
-  //       />
-  it("Learning Project renders", () => {
+  it("Learning Project render", () => {
     renderer.create(
       <Learning_Project
         actions={{
@@ -23,5 +22,43 @@ describe("Stateless Components Render", () => {
         }}
       />
     );
+  });
+});
+
+// Expected array but received object
+describe("CounterReducer test", () => {
+  it("should return initial state", () => {
+    expect(CounterReducer(undefined, {})).toBe([
+      {
+        count: 0,
+        count2: 0
+      }
+    ]);
+  });
+});
+
+describe("CounterReducer test", () => {
+  it("should handle Increment on count2", () => {
+    expect(
+      CounterReducer({ count: 0, count2: 0 }, { type: counterIncrement2 })
+    ).toEqual([
+      {
+        count1: 0,
+        count2: 1
+      }
+    ]);
+  });
+});
+
+describe("CounterReducer test", () => {
+  it("should handle Decrement on count1", () => {
+    expect(
+      CounterReducer({ count: 3, count2: 0 }, { type: counterDecrement })
+    ).toEqual([
+      {
+        count1: 2,
+        count2: 0
+      }
+    ]);
   });
 });
