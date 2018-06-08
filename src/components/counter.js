2beee60ec60b611c8be4f116ded5539d46cf1491
Jeyoung Park <jeyoungp@andrew.cmu.edu>
2018-06-08 08:51:56 -0400

    changed the styles to have own variables to be passed in

--- src/components/counter.js
+++ src/components/counter.js
@@ -13,18 +13,19 @@ import {
 } from "../actions/counter";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 30,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#000080"
-  },
-  buttons: {
-    flex: 1,
+  counter_row_first: {
     padding: 20,
     flexDirection: "row",
     justifyContent: "center"
+  },
+  counter_row: {
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  counter_col: {
+    flex: 1,
+    flexDirection: "column"
   },
   text: {
     fontFamily: "Roboto-Light",
@@ -38,51 +39,29 @@ const styles = StyleSheet.create({
 export class Learning_Project extends Component {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column"
-        }}
-      >
-        <View
-          style={{
-            padding: 20,
-            flexDirection: "row",
-            justifyContent: "center"
-          }}
-        >
+      <View style={styles.counter_col}>
+        <View style={styles.counter_row_first}>
           <Button
             large
             title="Increment +"
             color="green"
             onPress={this.props.actions.counterIncrement}
           />
-          <Text>
-            <Text style={styles.text}>{this.props.count}</Text>
-          </Text>
+          <Text style={styles.text}>{this.props.count}</Text>
           <Button
             large
             title="Decrement -"
             onPress={this.props.actions.counterDecrement}
           />
         </View>
-
-        <View
-          style={{
-            padding: 5,
-            flexDirection: "row",
-            justifyContent: "center"
-          }}
-        >
+        <View style={styles.counter_row}>
           <Button
             large
             title="Increment +"
             color="green"
             onPress={this.props.actions.counterIncrement2}
           />
-          <Text>
-            <Text style={styles.text}>{this.props.count2}</Text>
-          </Text>
+          <Text style={styles.text}>{this.props.count2}</Text>
           <Button
             large
             title="Decrement -"
